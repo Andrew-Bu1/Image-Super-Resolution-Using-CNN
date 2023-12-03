471ed03d3827d2b39cd11a6151ca46f2aaf405b7
Andrew-Bu1 <andrewbuianh30@gmail.com>
2023-12-03 20:39:08 +0700

    add arguments

--- bin/main.py
+++ bin/main.py
@@ -1,5 +1,15 @@
 import models
 import argparse
+import os
+
+
+def check_valid_file(path):
+    if (os.path.isfile(path)):
+        return path
+    else:
+        raise argparse.ArgumentError(
+            "This path {:s} is not a valid file, check again.".format(path))
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Main argument parser")
@@ -9,3 +19,10 @@ if __name__ == "__main__":
                         help="The type of model to be ran")
     parser.add_argument("--model_dir", type=str,
                         required=True, help="Location of model")
+    parser.add_argument("--config", type=str, nargs="+",
+                        default=None, help="Location of the config file")
+    # arguments for inference
+    parser.add_argument("--features_file", type=str,
+                        help="Inference mode: Provide the location of features file")
+    parser.add_argument("--predictions_file", type=str,
+                        help="Inference mode: Provide Location of output file which is predicted from features file")
--- models/SRCNN.py
+++ models/SRCNN.py
@@ -5,3 +5,9 @@ import torch.nn as nn
 class SRCNN(nn.Module):
     def __init__(self) -> None:
         super().__init__()
+
+    def run_train(self, **kwargs):
+        print("Model in training, with args: {}".format(kwargs))
+
+    def run_eval(self, **kwargs):
+        print("Model in evaluation, with args: {}".format(kwargs))
